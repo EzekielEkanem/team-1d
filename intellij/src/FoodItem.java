012c1ca8a685cfeaf0ec01793fff1f326a66929c
Aly Camara <acamara@efcodd.cs.vassar.edu>
2024-10-30 14:53:17 -0400

    built more of FoodItem class

--- intellij/src/FoodItem.java
+++ intellij/src/FoodItem.java
@@ -1,12 +1,51 @@
 package intellij.src;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 import static java.lang.System.out;
 
 public class FoodItem {
+    private String id;
+    private String name;
+    private HashSet<String> dietLabels;
+
+    public FoodItem(String id, JSONObject value){
+        this.id = id;
+        this.name = (String) value.get("label");
+        this.dietLabels = new HashSet<String>();
+
+        //out.println((JSONObject) value.get("cor_icon"));
+
+        // looping through the object to find the cor_icon dictionary
+        for (Object valueKey : value.keySet()) {
+            String valueKeyStr = (String) valueKey;
+
+            if (valueKeyStr.equals("cor_icon")){
+                // If the icons is not empty then
+                try {
+                    JSONObject icons = (JSONObject) value.get(valueKeyStr);
+
+                    if (icons != null){
+                        for (Object iconKey : icons.keySet()) {
+                            String iconKeyStr = (String) iconKey;
+
+                            String dietRest = (String) icons.get(iconKeyStr);
+
+                            dietLabels.add(dietRest);
+                        }
+                    }
+                } catch(Exception e) {
+                    continue;
+                }
+            }
+        }
+
+        out.println(this.dietLabels);
+    }
+
+
 
 }
+
--- intellij/src/Menu.java
+++ intellij/src/Menu.java
@@ -4,22 +4,30 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
 
 import static java.lang.System.out;
 
 public class Menu {
     JSONObject jsonMenuObject;
-    HashMap<Integer, JSONObject> mealDayParts;
+    HashMap<String, Object> FoodItems = new HashMap<String, Object>();
     public Menu(String urlStr) throws ParseException {
         Request request = new Request(urlStr);
         request.getWebPage();
         this.jsonMenuObject = request.getJsonMenu();
-        this.mealDayParts = request.getMealDayParts();
     }
 
-    public boolean getMenu() {
-        out.println("Menu Options for the day");
-        out.println(mealDayParts);
+    public <JSONElement> boolean getMenu() {
+        for (Object key : jsonMenuObject.keySet()) {
+            String keyStr = (String) key;
+            JSONObject value = (JSONObject) jsonMenuObject.get(keyStr);
+
+            //System.out.println(keyStr + " : " + value);
+
+            FoodItem newFood = new FoodItem(keyStr, value);
+        }
         return false;
     }
 }
