b741e084dafc435b5d3072980efda50fc89825d7
Aly Camara <acamara@acl204.cs.vassar.edu>
2024-12-09 21:04:10 -0500

    Created the system tests

--- astudio/VassarEats/app/src/androidTest/java/edu/vassar/cmpu203/vassareats/SelectPreferenceViewTest.java
+++ astudio/VassarEats/app/src/androidTest/java/edu/vassar/cmpu203/vassareats/SelectPreferenceViewTest.java
@@ -4,7 +4,6 @@ import android.os.SystemClock;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -12,24 +11,37 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
-import edu.vassar.cmpu203.vassareats.MainActivity;
-import edu.vassar.cmpu203.vassareats.model.DiningStation;
-import edu.vassar.cmpu203.vassareats.model.FoodItem;
-import edu.vassar.cmpu203.vassareats.model.MealType;
-import edu.vassar.cmpu203.vassareats.model.MealTypeSection;
-import edu.vassar.cmpu203.vassareats.model.Preference;
-
 public class SelectPreferenceViewTest {
 
     @Test
-    public void testHierarchicalMenuCreationAndDisplay() {
+    public void testMenuCreationAndDisplay() {
         // Launch the activity using ActivityScenario
         try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
 
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Apply")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            // Verify the menu is displayed in the UI
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealType_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealTypeSection_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("diningStation_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("foodItem_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        }
+    }
+
+    @Test
+    public void testMenuUpdateAndDisplay() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
             Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
 
             Espresso.onView(ViewMatchers.withText("Apply")).perform(ViewActions.click());
@@ -61,6 +73,85 @@ public class SelectPreferenceViewTest {
                     .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
             Espresso.onView(ViewMatchers.withTagValue(Matchers.is("foodItem_8")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        }
+    }
+
+    @Test
+    public void testMenuScroll() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Farm to Fork")).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Apply")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            Espresso.onView(ViewMatchers.withId(R.id.scrollView))
+                    .perform(ViewActions.swipeUp());
+
+            // Verify the menu is displayed in the UI
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealType_4")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        }
+    }
+
+    @Test
+    public void testMenuClearAllButton() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Farm to Fork")).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Apply")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Clear All")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealType_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealTypeSection_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("diningStation_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("foodItem_1")))
+                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        }
+    }
+
+    @Test
+    public void testMenuCancelButton() {
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Farm to Fork")).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Apply")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            Espresso.onView(ViewMatchers.withId(R.id.preference)).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Farm to Fork")).perform(ViewActions.click());
+
+            Espresso.onView(ViewMatchers.withText("Cancel")).perform(ViewActions.click());
+
+            SystemClock.sleep(1000);
+
+            Espresso.onView(ViewMatchers.withId(R.id.scrollView))
+                    .perform(ViewActions.swipeUp());
+
+            // Verify the menu is displayed in the UI
+            Espresso.onView(ViewMatchers.withTagValue(Matchers.is("mealType_4")))
                     .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         }
     }
