4e12ba3e3dbfae0b72d5e49b28e15f023bd0318e
Aly Camara <acamara@acl204.cs.vassar.edu>
2024-12-13 17:03:35 -0500

    Updated menu unit test

--- astudio/VassarEats/app/src/test/java/edu/vassar/cmpu203/vassareats/model/MenuTest.java
+++ astudio/VassarEats/app/src/test/java/edu/vassar/cmpu203/vassareats/model/MenuTest.java
@@ -6,6 +6,7 @@ import org.json.JSONException;
 import org.junit.Test;
 
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,24 +31,47 @@ public class MenuTest extends TestCase {
     }
 
     /**
-     * Tests the toString() method with the beginning of the menu
+     * Tests the toString() method by checking if the string contains the correct menu items
      */
     @Test
     public void testTestToString() throws JSONException, ParseException {
         Menu menu = new Menu();
 
-        assertTrue(menu.toString().contains("**************************************************************************\n" +
-                "                               Menu Items                                 \n" +
-                "**************************************************************************\n" +
-                "                               Breakfast                                 \n" +
-                "**************************************************************************\n" +
-                "              Breakfast Specials                          \n" +
-                "***********************************************************\n" +
-                "          Pressed                   \n" +
-                "********************************************\n" +
-                "Food id: 26946941\n" +
-                "Food name: make your own belgian waffle\n" +
-                "Dietary labels: [Vegetarian]\n" +
-                "*****************************"));
+        String dateString = "2024-12-13";
+
+        // Convert the string to a LocalDate
+        LocalDate localDate = LocalDate.parse(dateString);
+
+        menu.updateDate(localDate);
+
+        assertTrue(menu.toString().contains("oatmeal"));
+        assertTrue(menu.toString().contains("cream of rice"));
+        assertTrue(menu.toString().contains("cinnamon and vanilla french toast stuffed with blueberry cream cheese"));
+    }
+
+    /**
+     * Tests the updateDate() method by checking if the menus are different after updating the date
+     */
+    @Test
+    public void testUpdateDate() throws JSONException, ParseException {
+        Menu menu = new Menu();
+
+        String dateString1 = "2024-12-12";
+        String dateString2 = "2024-12-13";
+
+        // Convert the string to a LocalDate
+        LocalDate localDate1 = LocalDate.parse(dateString1);
+        LocalDate localDate2 = LocalDate.parse(dateString2);
+
+        menu.updateDate(localDate1);
+
+        String menu1 = menu.getMenu().toString();
+
+        menu.updateDate(localDate2);
+
+        String menu2 = menu.getMenu().toString();
+
+        assertTrue(menu1.contains("cream of wheat"));
+        assertFalse(menu2.contains("cream of wheat"));
     }
 }
